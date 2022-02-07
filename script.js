b56962cc2dafc1f1e3223155bafc148aa1a595f8
raashi_112 <rsharma@7classes.com>
2022-02-07 16:53:52 +0530

    First Commit

--- script.js
+++ script.js
@@ -23,9 +23,11 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    canvas.getContext('2d').clearRect(1, 1, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    /*faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)*/
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
+
+
